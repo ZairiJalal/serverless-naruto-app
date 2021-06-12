d1f5a281031ee1608d6aaf28ab6ec8d241c47016
ZairiJalal <zairi.jalal98@gmail.com>
2021-06-12 16:10:36 +0200

    S3-CloudFunctions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireMessagingModule } from '@angular/fire/messaging';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
-
-import { FormsModule } from '@angular/forms';
-import { AuthService } from './service/auth.service';
-
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,19 +10,9 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    AngularFireMessagingModule,
-    AngularFireStorageModule,
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [
-    AngularFirestore,
-    AuthService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase : {
-    apiKey: "AIzaSyDrIF3d1reVdv33yPKd2S-SSKL5oMrncFk",
-    authDomain: "backend--as-a--service.firebaseapp.com",
-    projectId: "backend--as-a--service",
-    storageBucket: "backend--as-a--service.appspot.com",
-    messagingSenderId: "30587456372",
-    appId: "1:30587456372:web:617dc96106cab41677ba21",
-    measurementId: "G-3XEBG7Q5BY"
-  }
+  production: false
 };
 
 /*
