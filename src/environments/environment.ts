9101de7f0ff822941054ea875033e8509a589efd
Zairi Jalal <zairi.jalal98@gmail.com>
2021-07-06 01:44:00 +0200

    S2-AuthWithFirebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,14 @@
 
 export const environment = {
   production: false,
-  firebase : {
-    apiKey: 'AIzaSyDdm0h6EtwMcgIkYGNjaWzJ_jluxSzz6_M',
-    authDomain: 'cours-tp.firebaseapp.com',
-    projectId: 'cours-tp',
-    storageBucket: 'cours-tp.appspot.com',
-    messagingSenderId: '954543312031',
-    appId: '1:954543312031:web:c36d681fbbc7c46fd166aa'
+  firebase :{
+    apiKey: "AIzaSyDrIF3d1reVdv33yPKd2S-SSKL5oMrncFk",
+    authDomain: "backend--as-a--service.firebaseapp.com",
+    projectId: "backend--as-a--service",
+    storageBucket: "backend--as-a--service.appspot.com",
+    messagingSenderId: "30587456372",
+    appId: "1:30587456372:web:617dc96106cab41677ba21",
+    measurementId: "G-3XEBG7Q5BY"
   }
 };
 
